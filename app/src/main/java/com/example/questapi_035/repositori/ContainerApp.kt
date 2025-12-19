98159080af4fe5061d3dcbc1a861f009b68a1d87
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-12-20 06:11:10 +0700

    Berfungsi sebagai kontrak dependency container yang menyediakan repository data

--- app/src/main/java/com/example/questapi_035/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_035/repositori/ContainerApp.kt
@@ -1,1 +1,14 @@
 package com.example.questapi_035.repositori
+
+import android.app.Application
+import com.example.questapi_035.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa : RepositoryDataSiswa
+}
