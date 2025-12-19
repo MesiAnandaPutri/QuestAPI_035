0be05d41023ea3621b26f0309e7fc5d018c2e00b
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-12-20 06:28:45 +0700

    mempresentasikan kondisi ui pada halaman home

--- app/src/main/java/com/example/questapi_035/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi_035/viewmodel/HomeViewModel.kt
@@ -1,1 +1,18 @@
 package com.example.questapi_035.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_035.modeldata.DataSiswa
+import com.example.questapi_035.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
