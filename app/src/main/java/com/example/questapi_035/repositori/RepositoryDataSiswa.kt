af1d0fb6f5d2a00def06c7088d0380629eaf475b
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-12-20 06:14:16 +0700

    Berfungsi sebagai kontrak repository untuk operasi data siswa

--- app/src/main/java/com/example/questapi_035/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_035/repositori/RepositoryDataSiswa.kt
@@ -1,1 +1,9 @@
 package com.example.questapi_035.repositori
+
+import com.example.questapi_035.apiservice.ServiceApiSiswa
+import com.example.questapi_035.modeldata.DataSiswa
+
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
