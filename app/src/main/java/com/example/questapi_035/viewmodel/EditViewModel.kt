6797da59cb29b73e98802b8af9946a338af080c6
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-12-26 22:53:39 +0700

    update state ui saat input berubah

--- app/src/main/java/com/example/questapi_035/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_035/viewmodel/EditViewModel.kt
@@ -32,3 +32,8 @@ class EditViewModel(
         }
     }
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
